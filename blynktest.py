c30a1a041a2433b4a0b623caba1e49ca1de2129f
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-09-26 23:29:38 +0700

    change

--- blynktest.py
+++ blynktest.py
@@ -1,9 +1,34 @@
 import blynklib
+import time
+import board
+import adafruit_dht
+
+dhtDevice = adafruit_dht.DHT22(board.D24)
 
 BLYNK_AUTH = 'nD-SwPo3-WpMrvAbdksIFa4YnP14l9-A'
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
 while True:
     blynk.run()
+    try:
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9/5) + 32
+        humidity = dhtDevice.humidity
+        print(
+            "Temp: {:.1f} F / {:.1f} C Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
+
+#     time.sleep(2.0)
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    time.sleep(2.0)
     blynk.virtual_write(1, 25.6)
     blynk.virtual_write(2, 90.6)
