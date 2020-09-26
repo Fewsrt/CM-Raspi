87d91b79e412d0978412fd8ca9459694728a2fa7
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-09-26 23:14:15 +0700

    change

--- blynktest.py
+++ blynktest.py
@@ -10,25 +10,4 @@ blynk = blynklib.Blynk(BLYNK_AUTH)
 
 while True:
     blynk.run()
-
-    try:
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9/5) + 32
-        humidity = dhtDevice.humidity
-        blynk.virtual_wirte(1, str(temperature_c))
-        print(
-            "Temp: {:.1f} F / {:.1f} C Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
-            )
-        )
-
-#     time.sleep(2.0)
-    except RuntimeError as error:
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
-
-    time.sleep(2.0)
+    blynk.virtual_wirte(1)
