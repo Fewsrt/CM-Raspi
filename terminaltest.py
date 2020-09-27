a502239353e9a688bf69131b3245cef32e10b6b3
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-09-27 22:02:43 +0700

    update

--- terminaltest.py
+++ terminaltest.py
@@ -1,4 +1,11 @@
 import blynklib
+import subprocess
+
+BLYNK_AUTH = 'YourAuthToken'
+
+# last command in example - just to show error handling
+# for certain HW can be added specific commands. 'gpio readall' on PI3b for example
+ALLOWED_COMMANDS_LIST = ['ls', 'lsusb', 'ip a', 'ip abc']
 
 # cmsensor1
 #BLYNK_AUTH = 'nD-SwPo3-WpMrvAbdksIFa4YnP14l9-A'
@@ -7,8 +14,36 @@ import blynklib
 BLYNK_AUTH = 'JFDPBMufAg2aRnHmO5ITI9H29aUbZmA1'
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
-WidgetTerminal terminal(V11);
 
+@blynk.handle_event('write V11')
+def write_handler(pin, values):
+    header = ''
+    result = ''
+    delimiter = '{}\n'.format('=' * 30)
+    if values and values[0] in ALLOWED_COMMANDS_LIST:
+        cmd_params = values[0].split(' ')
+        try:
+            result = subprocess.check_output(cmd_params).decode('utf-8')
+            header = '[output]\n'
+        except subprocess.CalledProcessError as exe_err:
+            header = '[error]\n'
+            result = 'Return Code: {}\n'.format(exe_err.returncode)
+        except Exception as g_err:
+            print("Command caused '{}'".format(g_err))
+    elif values and values[0] == 'help':
+        header = '[help -> allowed commands]\n'
+        result = '{}\n'.format('\n'.join(ALLOWED_COMMANDS_LIST))
+
+    # communicate with terminal if help or some allowed command
+    if result:
+        output = '{}{}{}{}'.format(header, delimiter, result, delimiter)
+        print(output)
+        blynk.virtual_write(pin, output)
+        blynk.virtual_write(pin, '\n')
+
+
+###########################################################
+# infinite loop that waits for event
+###########################################################
 while True:
     blynk.run()
-    terminal.println(F("Blynk v" BLYNK_VERSION ": Device started"))
