21fdc2924ad0111c3e1c2722f92bd95d68fa16eb
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-09-27 21:42:51 +0700

    update

--- dht22.py
+++ dht22.py
@@ -3,15 +3,22 @@ import board
 import blynklib
 import adafruit_dht
 
-dhtDevice = adafruit_dht.DHT22(board.D24)
-BLYNK_AUTH = 'nD-SwPo3-WpMrvAbdksIFa4YnP14l9-A'
+#cmsensor1
+#BLYNK_AUTH = 'nD-SwPo3-WpMrvAbdksIFa4YnP14l9-A'
+
+#cmsensor2
+BLYNK_AUTH = 'JFDPBMufAg2aRnHmO5ITI9H29aUbZmA1'
 blynk = blynklib.Blynk(BLYNK_AUTH)
+
+dhtDevice = adafruit_dht.DHT22(board.D24)
 
 while True:
     try:
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9/5) + 32
         humidity = dhtDevice.humidity
+        blynk.virtual_write(1, temperature_c)
+        blynk.virtual_write(2, humidity)
         print(
             "Temp: {:.1f} F / {:.1f} C Humidity: {}% ".format(
                 temperature_f, temperature_c, humidity
