264026407ab436105e8e2ace421f151e42db2e70
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-09-27 19:54:17 +0700

    update

--- iptest.py
+++ iptest.py
@@ -1,7 +1,17 @@
 import socket
-testIP = "8.8.8.8"
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect((testIP, 0))
-ipaddr = s.getsockname()[0]
-host = socket.gethostname()
-print ("IP:", ipaddr, " Host:", host)
+import time
+import blynklib
+
+BLYNK_AUTH = 'nD-SwPo3-WpMrvAbdksIFa4YnP14l9-A'
+blynk = blynklib.Blynk(BLYNK_AUTH)
+
+while True:
+    blynk.run()
+    testIP = "8.8.8.8"
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((testIP, 0))
+    ipaddr = s.getsockname()[0]
+    host = socket.gethostname()
+    blynk.virtual_write(5, str(ipaddr))
+    print ("IP:", ipaddr, " Host:", host)
+    time.sleep(3)
