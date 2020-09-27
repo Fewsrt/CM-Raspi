3043e9aada4f60e3e51622184b9d5d1f1fe2b0e3
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-09-27 20:43:46 +0700

    update

--- waterdetect.py
+++ waterdetect.py
@@ -6,17 +6,8 @@ channel = 23
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
-
-def callback(channel):
+while True:
     if GPIO.input(channel):
         print("No Water Detected!")
-
     else:
         print("Water Detected!")
-
-
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
-GPIO.add_event_callback(channel, callback)
-
-while True:
-    time.sleep(2)
