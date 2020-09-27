c591d883eb79195eb077c4e2c97e591d39fd4fa7
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-09-27 22:04:55 +0700

    update

--- terminaltest.py
+++ terminaltest.py
@@ -5,7 +5,7 @@ BLYNK_AUTH = 'YourAuthToken'
 
 # last command in example - just to show error handling
 # for certain HW can be added specific commands. 'gpio readall' on PI3b for example
-ALLOWED_COMMANDS_LIST = ['ls', 'lsusb', 'ip a', 'ip abc']
+ALLOWED_COMMANDS_LIST = ['ls', 'lsusb', 'ip a', 'ip abc','i2c detect -y 1']
 
 # cmsensor1
 #BLYNK_AUTH = 'nD-SwPo3-WpMrvAbdksIFa4YnP14l9-A'
