0e7db34829d6971b341388dc4bded6b937900d48
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-09-27 20:33:57 +0700

    update

---
+++ waterdetect.py
@@ -0,0 +1,24 @@
+import blynklib
+import RPi.GPIO as GPIO
+
+BLYNK_AUTH = 'nD-SwPo3-WpMrvAbdksIFa4YnP14l9-A'
+blynk = blynklib.Blynk(BLYNK_AUTH)
+
+channel = 23
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+
+def callback(channel):
+    if GPIO.input(channel):
+        print("No Water Detected!")
+        blynk.virtual_write(0, "No Water Detected!")
+        blynk.virtual_write(1, 0)
+    else:
+        print("Water Detected!")
+        blynk.virtual_write(0, "Water Detected!")
+        blynk.virtual_write(1, 255)
+
+
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(channel, callback)
