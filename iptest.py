6754c13cc009fb63ce2c86b4e1d03e8abc37dd8f
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-09-27 20:05:04 +0700

    update

--- iptest.py
+++ iptest.py
@@ -1,12 +1,17 @@
 import socket
 import time
 import blynklib
+from gpiozero import CPUTemperature
 
 BLYNK_AUTH = 'nD-SwPo3-WpMrvAbdksIFa4YnP14l9-A'
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
 while True:
     blynk.run()
+    readip(ipaddr, host)
+
+
+def readip(ipaddr, host):
     testIP = "8.8.8.8"
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect((testIP, 0))
